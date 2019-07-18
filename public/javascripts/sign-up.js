3fa33b46ec10c3a850d5fc3a1f81d7be83cf8fb5
mukeunzi <eunji057@gmail.com>
2019-07-18 21:39:48 +0900

    feat : id/pwd 유효성 검사 메소드 추가
    - id/pwd 가 비어있을 경우 리턴

--- public/javascripts/sign-up.js
+++ public/javascripts/sign-up.js
@@ -9,6 +9,26 @@ const load = () => {
 	});
 };
 
-const isValidSignUpData = () => {};
+const isValidSignUpData = () => {
+	const user_id = document.querySelector('#user_id').value;
+	const user_pwd = document.querySelector('#user_pwd').value;
+
+	if (isValidId(user_id) && isValidPassword(user_pwd)) {
+	}
+};
+
+const isValidId = user_id => {
+	if (!user_id) {
+		return document.querySelector('#user_id').focus();
+	}
+	return true;
+};
+
+const isValidPassword = user_pwd => {
+	if (!user_pwd) {
+		return document.querySelector('#user_pwd').focus();
+	}
+	return true;
+};
 
 load();
