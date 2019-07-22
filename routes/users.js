b8afd551b896df14df08919acac6ff6f7302dfbc
mukeunzi <eunji057@gmail.com>
2019-07-22 16:00:28 +0900

    feat : GET /users 라우터 추가
    - 회원가입 페이지

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const User = require('../models/user');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+	res.render('sign-up', { title: 'Sign Up' });
 });
 
 module.exports = router;
