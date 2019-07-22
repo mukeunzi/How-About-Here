17ab5dac64ba9e0cc2320575603e5ab6d93cf4bd
mukeunzi <eunji057@gmail.com>
2019-07-22 16:02:10 +0900

    feat : POST /users API 추가
    - 회원가입 라우터
    - 역할 : 아이디 유효성검사, 회원가입

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,32 @@ router.get('/', (req, res, next) => {
 	res.render('sign-up', { title: 'Sign Up' });
 });
 
+router.post('/', async (req, res, next) => {
+	const { user_id, user_password } = req.body;
+
+	try {
+		const duplicatedId = await User.checkDuplicatedId(user_id);
+
+		if (duplicatedId) {
+			return res.redirect('/users');
+		}
+
+		const user = new User({
+			user_id,
+			user_password,
+			user_auth: 'user',
+			user_score: 0,
+			user_ranking: 0
+		});
+
+		const createUser = await user.save();
+
+		if (createUser) {
+			return res.redirect('/');
+		}
+		next(500);
+	} catch (error) {
+		next(error);
+	}
+});
 module.exports = router;
