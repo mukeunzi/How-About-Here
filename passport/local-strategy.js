3b8eff3a79b41173e30f58eb2b1772d280f1f96e
mukeunzi <eunji057@gmail.com>
2019-07-22 21:46:32 +0900

    feat : passport local strategy 구현
    - 로컬 passport 구현

---
+++ passport/local-strategy.js
@@ -0,0 +1,34 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+const User = require('../models/user');
+
+module.exports = passport => {
+	passport.use(
+		new LocalStrategy(
+			{
+				usernameField: 'user_id',
+				passwordField: 'user_password'
+			},
+			async (user_id, user_password, done) => {
+				try {
+					const userInfo = await User.findOne({ user_id });
+
+					if (!userInfo) {
+						return done(null, false, { message: '아이디나 비밀번호가 올바르지 않습니다.' });
+					}
+
+					const isValidUser = await bcrypt.compare(user_password, userInfo.user_password);
+
+					if (!isValidUser) {
+						return done(null, false, { message: '비밀번호가 올바르지 않습니다.' });
+					}
+					return done(null, userInfo);
+				} catch (error) {
+					console.error(error);
+					return done(error);
+				}
+			}
+		)
+	);
+};
