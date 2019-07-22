0bff3c2712fc5fe5ab93af34af86f44937a58df4
mukeunzi <eunji057@gmail.com>
2019-07-22 17:51:41 +0900

    feat : auth 라우터 추가
    - 로그인 페이지 GET /auth 추가

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongodb = require('./models/index');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 
 mongodb();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
