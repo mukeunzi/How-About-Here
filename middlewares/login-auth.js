eebe214700f2846f49bb8a0ca082235942d87ca5
mukeunzi <eunji057@gmail.com>
2019-07-23 11:21:20 +0900

    feat : 로그인 인증 메소드 추가
    - isAuthenticated() 메소드를 이용한 로그인/로그아웃 을 인증하는 메소드
    생성

---
+++ middlewares/login-auth.js
@@ -0,0 +1,15 @@
+const isLoggedIn = (req, res, next) => {
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	res.status(403).send('로그인이 필요합니다.');
+};
+
+const isLoggedOut = (req, res, next) => {
+	if (!req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect('/');
+};
+
+module.exports = { isLoggedIn, isLoggedOut };
