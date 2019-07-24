3d3dedcf0f3e053125f2fd77f8a5d406f5a23646
mukeunzi <eunji057@gmail.com>
2019-07-24 16:38:05 +0900

    feat : 로그인 api 추가
    - localStrategy 사용

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const { isLoggedOut } = require('../middlewares/login-auth');
 
 router.get('/', isLoggedOut, function(req, res, next) {
 	res.render('login', { title: 'LOGIN' });
 });
+
+router.post(
+	'/',
+	isLoggedOut,
+	passport.authenticate('local', { failureRedirect: '/auth', failureFlash: true }),
+	function(req, res, next) {
+		res.render('index');
+	}
+);
 
 module.exports = router;
