41ffa05b18de6f2e1aac0eab32261f262574ead4
mukeunzi <eunji057@gmail.com>
2019-07-27 00:42:30 +0900

    feat : session을 이용한 로그인/로그아웃/회원가입 버튼 생성
    - res.locals 변수에 req.user 할당
    - index.pug 에서 user 가 있으면 로그아웃 버튼, 없으면 로그인/회원가입
    버튼 생성

--- app.js
+++ app.js
@@ -37,6 +37,11 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use((req, res, next) => {
+	res.locals.user = req.user || null;
+
+	next();
+});
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
