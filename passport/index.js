2d28f80b8df3714dd787d117a710d880cbe12b28
mukeunzi <eunji057@gmail.com>
2019-07-30 03:22:37 +0900

    fix : deserializeUser 누락된 코드 수정
    - user_id 로 회원 정보를 불러오는 코드 추가

--- passport/index.js
+++ passport/index.js
@@ -1,11 +1,14 @@
 const localStrategy = require('./local-strategy');
+const User = require('../models/user');
 
 module.exports = passport => {
 	passport.serializeUser((user, done) => {
 		done(null, user.user_id);
 	});
 
-	passport.deserializeUser(async (user, done) => {
+	passport.deserializeUser(async (user_id, done) => {
+		const user = await User.getUserInfo(user_id);
+
 		done(null, user);
 	});
 
