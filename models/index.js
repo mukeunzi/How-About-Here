1c4847b05055d7c82b5e7a6e67f74085146934a5
mukeunzi <eunji057@gmail.com>
2019-07-30 19:38:23 +0900

    refactor : mongodb connection 코드 수정
    - (참고 : MongoDB 에서 deprecated 된 ensureIndex() warning 을 방지하기
    위해 useCreateIndex() 추가)

--- models/index.js
+++ models/index.js
@@ -1,35 +1,14 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-module.exports = () => {
-	const connect = () => {
-		if (process.env.NODE_ENV !== 'production') {
-			mongoose.set('debug', true);
-		}
+const mongoURI = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
-		mongoose.connect(
-			`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${
-				process.env.DB_ADMIN
-			}`,
-			{
-				useNewUrlParser: true,
-				dbName: process.env.DB_NAME
-			},
-			error => {
-				if (error) {
-					process.exit(1);
-				}
-			}
-		);
-	};
-
-	connect();
-
-	mongoose.connection.on('error', error => {
-		console.error('몽고디비 연결 오류!!', error);
-	});
-
-	mongoose.connection.on('open', () => {
-		console.log('몽고디비 연결 성공!!');
-	});
+module.exports = async () => {
+	try {
+		await mongoose.connect(mongoURI, { useNewUrlParser: true, useCreateIndex: true });
+		console.log('Connected to mongodb...');
+	} catch (error) {
+		console.error('Could not connect to mongodb!!! : ', error);
+		return process.exit(1);
+	}
 };
