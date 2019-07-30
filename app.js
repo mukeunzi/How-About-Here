9596dffc212607cb442675880f148a74919068f3
mukeunzi <eunji057@gmail.com>
2019-07-30 19:51:17 +0900

    feat : 불필요한 주석 제거

--- app.js
+++ app.js
@@ -46,18 +46,14 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
 	next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// render the error page
 	res.status(err.status || 500);
 	res.render('error');
 });
