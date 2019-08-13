19b550e9d8923f5120ae51d8a43b4ff701a05841
mukeunzi <eunji057@gmail.com>
2019-08-13 21:43:04 +0900

    refactor : auth-controller 생성
    - googleLogin : googleapis 모듈을 이용해 oauth 인증을 하는 로그인 메소드
                    jwt 토큰을 이용해 cookie 에 access token 저장
    		req.user 에 로그인 된 회원의 id 저장
    - redirectGoogleLogin : oauth 인증 페이지로 리다이렉트 하는 메소드
    - logOut : session 과 cookie 를 삭제해 로그아웃 하는 메소드
    - getLogInPage : 로그인 페이지를 불러오는 메소드
    
    - jsonwebtoken 모듈 추가

---
+++ controllers/auth-controller.js
@@ -0,0 +1,49 @@
+const { url, googleLogIn } = require('../config/google-oauth');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+class AuthController {
+	async googleLogIn(req, res, next) {
+		try {
+			const googleUserData = await googleLogIn(req.query.code);
+			const { user_id, auth_provider } = googleUserData;
+
+			const duplicatedId = await User.checkDuplicatedId(user_id, auth_provider);
+
+			if (!duplicatedId) {
+				await User.signUp(googleUserData);
+			}
+
+			const token = await jwt.sign({ user_id, auth_provider }, process.env.JWT_SECRET);
+			res.cookie('token', token, { httpOnly: true, maxAge: 1000 * 60 * 60 });
+
+			req.user = user_id;
+
+			res.redirect('/');
+		} catch (error) {
+			next(error);
+		}
+	}
+
+	redirectGoogleLogIn(req, res) {
+		res.redirect(url);
+	}
+
+	logOut(req, res) {
+		req.logout();
+		req.session.destroy();
+		res.send('successLogOut');
+	}
+
+	getLogInPage(req, res) {
+		const flashMessage = req.flash();
+		let message = '';
+
+		if (flashMessage.error) {
+			message = flashMessage.error[0];
+		}
+		res.render('login', { title: 'LOGIN', message });
+	}
+}
+
+module.exports = new AuthController();
--- routes/auth.js
+++ routes/auth.js
@@ -1,44 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const User = require('../models/user');
 const { isLoggedIn, isNotLoggedIn } = require('../middlewares/login-auth');
-const { url, googleLogIn } = require('../config/google-oauth');
 const authController = require('../controllers/auth-controller');
 
-router.get('/', isNotLoggedIn, function(req, res) {
-	const flashMessage = req.flash();
-	let message = '';
-
-	if (flashMessage.error) {
-		message = flashMessage.error[0];
-	}
-	res.render('login', { title: 'LOGIN', message });
-});
-
+router.get('/', isNotLoggedIn, authController.getLogInPage);
 router.post(
 	'/',
 	isNotLoggedIn,
 	passport.authenticate('local', { successRedirect: '/', failureRedirect: '/auth', failureFlash: true })
 );
-
-router.delete('/', isLoggedIn, (req, res) => {
-	req.logout();
-	req.session.destroy();
-	res.send('successLogOut');
-});
-
-router.get('/google-login', isNotLoggedIn, (req, res) => {
-	res.redirect(url);
-});
-
-router.get('/google-oauth2', async (req, res) => {
-	try {
-		const displayName = await googleLogIn(req.query.code);
-		console.log(displayName);
-	} catch (error) {
-		console.error(error);
-	}
-});
+router.delete('/', isLoggedIn, authController.logOut);
+router.get('/google-login', isNotLoggedIn, authController.redirectGoogleLogIn);
+router.get('/google-oauth2', authController.googleLogIn);
 
 module.exports = router;
