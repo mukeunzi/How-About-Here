73520f4f0732e1744f50a074c3adf75855f8b3e0
mukeunzi <eunji057@gmail.com>
2019-08-16 16:31:32 +0900

    refactor : index 라우터 로직 분리

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { verifyToken } = require('../middlewares/verify-token');
+const IndexController = require('../controllers/index-controller');
 
-router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express' });
-});
+router.get('/', verifyToken, IndexController.getIndexPage);
 
 module.exports = router;
