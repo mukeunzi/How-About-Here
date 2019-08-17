283a6aea48ec5133065ea7356e0b3c5773d9b80a
mukeunzi <eunji057@gmail.com>
2019-08-17 16:11:39 +0900

    feat : 불필요한 코드 삭제

--- app.js
+++ app.js
@@ -38,11 +38,6 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use((req, res, next) => {
-	res.locals.user = req.user || null;
-
-	next();
-});
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
