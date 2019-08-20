42484a2d8da12bb77740fff9513c7a2724e5504e
mukeunzi <eunji057@gmail.com>
2019-08-20 16:54:28 +0900

    feat : localLogIn 메소드 생성
    - 로컬 로그인에 성공하면 jwt 토큰을 생성하는 메소드
    - jwtUtil 메소드 사용

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,12 +1,22 @@
 const { url, googleLogIn } = require('../config/google-oauth');
-const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const jwtUtil = require('../utils/jwt-token');
 
 class AuthController {
+	async localLogIn(req, res, next) {
+		try {
+			const token = await jwtUtil.makeToken(req.user);
+			res.cookie('token', token, { httpOnly: true, maxAge: 1000 * 60 * 60 });
+
+			return res.redirect('/');
+		} catch (error) {
+			next(error);
+		}
+	}
 	async googleLogIn(req, res, next) {
 		try {
 			const googleUserData = await googleLogIn(req.query.code);
-			const { user_id, auth_provider, user_auth } = googleUserData;
+			const { user_id, auth_provider } = googleUserData;
 
 			const duplicatedId = await User.checkDuplicatedId(user_id, auth_provider);
 
@@ -14,7 +24,7 @@ class AuthController {
 				await User.signUp(googleUserData);
 			}
 
-			const token = await jwt.sign({ user_id, auth_provider, user_auth }, process.env.JWT_SECRET);
+			const token = await jwtUtil.makeToken(googleUserData);
 			res.cookie('token', token, { httpOnly: true, maxAge: 1000 * 60 * 60 });
 
 			return res.redirect('/');
