34e8e20bd48b7882c94271ecdb6572e769aa1f70
mukeunzi <eunji057@gmail.com>
2019-08-22 16:24:27 +0900

    feat : postSchema 생성
    - 게시물 스키마 생성

---
+++ models/post.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const {
+	Type: { ObjectId }
+} = Schema;
+
+const postSchema = new Schema({
+	post_title: {
+		type: String,
+		required: true
+	},
+	first_candidate: {
+		type: String,
+		required: true
+	},
+	second_candidate: {
+		type: String,
+		required: true
+	},
+	first_counting: {
+		type: Number,
+		required: true,
+		default: 0
+	},
+	second_counting: {
+		type: Number,
+		required: true,
+		default: 0
+	},
+	voting_participant: {
+		type: Number,
+		required: true,
+		default: 0
+	},
+	create_id: {
+		type: ObjectId,
+		required: true,
+		ref: 'User'
+	},
+	create_date: {
+		type: Date,
+		required: true,
+		default: Date.now()
+	},
+	update_id: {
+		type: ObjectId,
+		required: true,
+		ref: 'User'
+	},
+	update_date: {
+		type: Date,
+		required: true,
+		default: Date.now()
+	}
+});
+
+module.exports = mongoose.model('Post', postSchema);
