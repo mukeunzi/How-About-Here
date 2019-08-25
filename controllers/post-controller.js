527460159aeb977d589e7af9bf07716d5dfacfb9
mukeunzi <eunji057@gmail.com>
2019-08-25 13:24:17 +0900

    feat : PostController 생성
    - createPost : 게시물을 작성하는 메소드

---
+++ controllers/post-controller.js
@@ -0,0 +1,16 @@
+const Post = require('../models/post');
+
+class PostController {
+	async createPost(req, res, next) {
+		try {
+			const authorObjectId = req.user._id;
+			await Post.createPost(authorObjectId, req.body);
+
+			return res.redirect('/');
+		} catch (error) {
+			next(error);
+		}
+	}
+}
+
+module.exports = new PostController();
