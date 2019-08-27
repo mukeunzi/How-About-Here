b4efd667666a36dd0aef8494121342f1b7175401
mukeunzi <eunji057@gmail.com>
2019-08-27 20:00:56 +0900

    feat : GET /post 라우터 추가

--- routes/post.js
+++ routes/post.js
@@ -3,6 +3,7 @@ const postController = require('../controllers/post-controller');
 
 const router = express.Router();
 
+router.get('/', postController.getPostPage);
 router.post('/', postController.createPost);
 router.patch('/', postController.updatePost);
 router.delete('/', postController.deletePost);
