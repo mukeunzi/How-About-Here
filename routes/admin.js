3b24850a61b5b9c0efc89afb734e7b7f38ddcec7
mukeunzi <eunji057@gmail.com>
2019-08-29 19:27:16 +0900

    feat : post 게시물을 불러오는 api 추가
    - /admin/post/list : 모든 게시물을 불러오는 api

--- routes/admin.js
+++ routes/admin.js
@@ -9,4 +9,6 @@ router.post('/post', postController.createPost);
 router.patch('/post', postController.updatePost);
 router.delete('/post', postController.deletePost);
 
+router.get('/post/list', isLoggedIn, postController.getPostListAll);
+
 module.exports = router;
