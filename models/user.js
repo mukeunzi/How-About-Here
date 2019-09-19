ed5d2f9a81c5b2454abc64af71aa09e5f0922016
mukeunzi <eunji057@gmail.com>
2019-09-19 14:22:12 +0900

    feat : user schema 변경
    - 프로젝트 변경으로 인 한 스키마 변경
    - 불필요한 도큐먼트 삭제 및 추가
    - 사용자 name 유효성체크를 위한 checkDuplicatedName() 메소드 추가

--- models/user.js
+++ models/user.js
@@ -4,10 +4,13 @@ const bcrypt = require('bcrypt');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
+	user_name: {
+		type: String,
+		required: true
+	},
 	user_id: {
 		type: String,
-		required: true,
-		unique: true
+		required: true
 	},
 	user_password: {
 		type: String,
@@ -18,14 +21,6 @@ const userSchema = new Schema({
 		required: true,
 		default: 'user'
 	},
-	user_score: {
-		type: Number,
-		required: true,
-		default: 0
-	},
-	user_ranking: {
-		type: Number
-	},
 	auth_provider: {
 		type: String,
 		required: true
@@ -33,21 +28,29 @@ const userSchema = new Schema({
 });
 
 userSchema.statics.signUp = async function(signUpForm) {
-	const { user_id, user_password, auth_provider } = signUpForm;
+	const { user_name, user_id, user_password, auth_provider } = signUpForm;
 	const hashPassword = await bcrypt.hash(user_password, 12);
 
 	await this.create({
+		user_name,
 		user_id,
 		user_password: hashPassword,
 		user_auth: 'user',
-		user_score: 0,
-		user_ranking: 0,
 		auth_provider
 	});
 };
 
 userSchema.statics.checkDuplicatedId = async function(user_id, auth_provider) {
 	const user = await this.findOne({ user_id, auth_provider });
+
+	if (user) {
+		return true;
+	}
+	return false;
+};
+
+userSchema.statics.checkDuplicatedName = async function(user_name) {
+	const user = await this.findOne({ user_name });
 
 	if (user) {
 		return true;
