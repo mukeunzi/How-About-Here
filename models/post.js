cad25bb631c069bd47c6515e393f9af8ee4432f2
mukeunzi <eunji057@gmail.com>
2019-09-25 20:52:33 +0900

    feat : post schema 생성
    - 사용자 게시물 관리를 위한 post schema 생성
    - 기존 코드 삭제 및 수정

--- models/post.js
+++ models/post.js
@@ -5,118 +5,53 @@ const {
 	Types: { ObjectId }
 } = Schema;
 
-const postSchema = new Schema({
-	post_title: {
-		type: String,
-		required: true
-	},
-	first_candidate: {
-		type: String,
-		required: true
-	},
-	second_candidate: {
-		type: String,
-		required: true
-	},
-	first_counting: {
-		type: Number,
-		required: true,
-		default: 0
-	},
-	second_counting: {
-		type: Number,
-		required: true,
-		default: 0
-	},
-	voting_participant: {
-		type: Number,
-		required: true,
-		default: 0
-	},
-	create_id: {
-		type: ObjectId,
-		required: true,
-		ref: 'User'
-	},
-	create_date: {
-		type: Date,
-		required: true,
-		default: Date.now
-	},
-	update_id: {
-		type: ObjectId,
-		required: true,
-		ref: 'User'
-	},
-	update_date: {
-		type: Date,
-		required: true,
-		default: Date.now
-	},
-	status_code: {
-		type: Number,
-		required: true,
-		default: 1
-	}
-});
-
-postSchema.statics.createPost = async function(authorObjectId, postForm) {
-	const { post_title, first_candidate, second_candidate } = postForm;
-
-	await this.create({
-		post_title,
-		first_candidate,
-		second_candidate,
-		create_id: authorObjectId,
-		update_id: authorObjectId
-	});
-};
-
-postSchema.statics.updatePost = async function(authorObjectId, postForm) {
-	const { _id, post_title, first_candidate, second_candidate, status_code } = postForm;
-
-	await this.updateOne(
-		{ _id },
-		{
-			$set: {
-				post_title,
-				first_candidate,
-				second_candidate,
-				update_id: authorObjectId,
-				update_date: Date.now(),
-				status_code
-			}
+const postSchema = new Schema(
+	{
+		business_name: {
+			type: String,
+			required: true
+		},
+		region_name: {
+			type: ObjectId,
+			required: true,
+			ref: 'Region'
+		},
+		detail_address: {
+			type: String,
+			required: true
+		},
+		post_contents: {
+			type: String,
+			required: true
+		},
+		star_rating: {
+			type: Number,
+			required: true,
+			default: 0
+		},
+		tag_list: {
+			type: [ObjectId],
+			required: true,
+			ref: 'Tag',
+			default: undefined
+		},
+		create_id: {
+			type: ObjectId,
+			required: true,
+			ref: 'User'
+		},
+		update_id: {
+			type: ObjectId,
+			required: true,
+			ref: 'User'
+		},
+		status_code: {
+			type: Number,
+			required: true,
+			default: 1
 		}
-	);
-};
-
-postSchema.statics.deletePost = async function(authorObjectId, postForm) {
-	const { _id, status_code } = postForm;
-
-	await this.updateOne(
-		{ _id },
-		{
-			$set: {
-				update_id: authorObjectId,
-				update_date: Date.now(),
-				status_code
-			}
-		}
-	);
-};
-
-postSchema.statics.getPostListAll = async function() {
-	const postList = await this.find()
-		.populate('create_id', 'user_id')
-		.populate('update_id', 'user_id');
-
-	return postList;
-};
-
-postSchema.statics.getPostDetailPage = async function(post_id) {
-	const postDetailPage = await this.find({ _id: post_id });
-
-	return postDetailPage;
-};
+	},
+	{ timestamps: { createdAt: 'create_date', updatedAt: 'update_date' } }
+);
 
 module.exports = mongoose.model('Post', postSchema);
