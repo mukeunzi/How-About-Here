5bdf353e3f7eacd8275c32c0119d3278accd7e0a
mukeunzi <eunji057@gmail.com>
2019-10-24 15:01:34 +0900

    feat : kakao map api를 이용한 게시물 상세페이지 위치 제공
    - 게시물 상세페이지 주소에 해당하는 위치 제공

---
+++ public/javascripts/kakao-map-detail.js
@@ -0,0 +1,36 @@
+const mapContainer = document.getElementById('map'); // 지도를 표시할 div
+const mapOption = {
+	center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+	level: 3 // 지도의 확대 레벨
+};
+
+// 지도를 생성합니다
+const map = new kakao.maps.Map(mapContainer, mapOption);
+
+// 주소-좌표 변환 객체를 생성합니다
+const geocoder = new kakao.maps.services.Geocoder();
+
+// 주소로 좌표를 검색합니다
+const detailAddress = document.querySelector('#detail_address').innerHTML;
+geocoder.addressSearch(detailAddress, function(result, status) {
+	// 정상적으로 검색이 완료됐으면
+	if (status === kakao.maps.services.Status.OK) {
+		const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+		// 결과값으로 받은 위치를 마커로 표시합니다
+		const marker = new kakao.maps.Marker({
+			map: map,
+			position: coords
+		});
+
+		// 인포윈도우로 장소에 대한 설명을 표시합니다
+		const placeName = document.querySelector('#place_name').innerHTML;
+		const infowindow = new kakao.maps.InfoWindow({
+			content: `<div style='width:150px;text-align:center;padding:6px 0;'>${placeName}</div>`
+		});
+		infowindow.open(map, marker);
+
+		// 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+		map.setCenter(coords);
+	}
+});
