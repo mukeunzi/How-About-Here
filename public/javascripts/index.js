a4671f45e577a8bd43bddb449ce633e1b7727777
mukeunzi <eunji057@gmail.com>
2019-10-29 16:33:28 +0900

    style : 페이지 헤더 ui 적용
    - semantic-ui ui library 사용
    - 모든 페이지에 공통으로 사용되는 헤더 구현
    - ui 변경으로 인한 버튼 이벤트 수정

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,31 +1,9 @@
 window.addEventListener('load', () => {
-	const logInButton = document.querySelector('#logInButton');
-	const signUpButton = document.querySelector('#signUpButton');
 	const logOutButton = document.querySelector('#logOutButton');
-	const postButton = document.querySelector('#postButton');
-	const adminButton = document.querySelector('#adminButton');
 
-	if (logOutButton && postButton) {
+	if (logOutButton) {
 		logOutButton.addEventListener('click', () => {
 			logOut();
-		});
-
-		postButton.addEventListener('click', () => {
-			location.href = '/post';
-		});
-
-		if (adminButton) {
-			adminButton.addEventListener('click', () => {
-				location.href = '/admin';
-			});
-		}
-	} else {
-		logInButton.addEventListener('click', () => {
-			location.href = '/auth';
-		});
-
-		signUpButton.addEventListener('click', () => {
-			location.href = '/users';
 		});
 	}
 });
