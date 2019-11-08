c9eda62b339652a4ffafbe7b5ac1aade2269c331
mukeunzi <eunji057@gmail.com>
2019-11-08 23:45:30 +0900

    fix : 댓글 삭제 권한 부여방식 변경 및 댓글 생성 시 삭제버튼 추가
    - 불충분한 테스트로 인한.. 댓글 삭제 권한 부여방식 변경
      로그인 되어있는 사용자(user._id, string)과 댓글
      작성자(comment.create_id._id.toString(), object => string)값으로 비교
    - 댓글 추가 시 생성하는 새로운 댓글 element 에서 누락된 삭제 버튼 추가

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -9,16 +9,24 @@ class CommentController {
 		const commentForm = { comment_body, post_id };
 
 		const newComment = await Comment.createComment(authorObjectId, commentForm);
-		const newCommentElement = `<div class="comment">
-			<a class="avatar"><img src="/images/profile.png"></a>
-			<div class="content">
-				<a class="author">${req.user.user_name}</a>
-				<div class="metadata">
-					<span class="date">${moment(newComment.create_date).fromNow()}</span>
+		let newCommentElement = `<div class='comment'>
+				<a class='avatar'>
+					<img src='/images/profile.png'>
+				</a>
+				<div class='content'>
+					<input class='comment_id' type='hidden' value=${newComment._id}>
+					<a class='author'>${req.user.user_name}</a>
+					<div class='metadata'>
+						<span class='date'>${moment(newComment.create_date).fromNow()}</span>`;
+
+		if (authorObjectId === newComment.create_id._id.toString()) {
+			newCommentElement += `<span class='deleteComment' style='cursor:pointer;'>삭제</span>`;
+		}
+
+		newCommentElement += `</div>
+					<div class='text'>${newComment.comment_body}</div>
 				</div>
-				<div class="text">${newComment.comment_body}</div>
-			</div>
-		</div>`;
+			</div>`;
 
 		return res.send(newCommentElement);
 	}
