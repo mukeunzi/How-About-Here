c058d8464025348aa4f1930352636c03a5d0c5db
mukeunzi <eunji057@gmail.com>
2019-12-28 13:58:48 +0900

    Feat : user 스키마 생성
    - 스키마 및 마이그레이션 파일 추가

---
+++ migrations/20191225093036-create-user.js
@@ -0,0 +1,50 @@
+'use strict';
+module.exports = {
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable('User', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER(8).UNSIGNED.ZEROFILL
+			},
+			userId: {
+				allowNull: false,
+				type: Sequelize.STRING(30)
+			},
+			userPassword: {
+				allowNull: false,
+				type: Sequelize.STRING(100)
+			},
+			userName: {
+				allowNull: false,
+				type: Sequelize.STRING(20)
+			},
+			userAuth: {
+				allowNull: false,
+				type: Sequelize.STRING(15),
+				defaultValue: 'user'
+			},
+			authProvider: {
+				allowNull: false,
+				type: Sequelize.STRING(15)
+			},
+			statusCode: {
+				allowNull: false,
+				type: Sequelize.INTEGER(1),
+				defaultValue: 1
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.dropTable('User');
+	}
+};
---
+++ schema/user.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+	const User = sequelize.define(
+		'User',
+		{
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: DataTypes.INTEGER(8).UNSIGNED.ZEROFILL
+			},
+			userId: {
+				allowNull: false,
+				type: DataTypes.STRING(30)
+			},
+			userPassword: {
+				allowNull: false,
+				type: DataTypes.STRING(100)
+			},
+			userName: {
+				allowNull: false,
+				type: DataTypes.STRING(20)
+			},
+			userAuth: {
+				allowNull: false,
+				type: DataTypes.STRING(15),
+				defaultValue: 'user'
+			},
+			authProvider: {
+				allowNull: false,
+				type: DataTypes.STRING(15)
+			},
+			statusCode: {
+				allowNull: false,
+				type: DataTypes.INTEGER(1),
+				defaultValue: '1'
+			},
+			createdAt: {
+				allowNull: false,
+				type: DataTypes.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: DataTypes.DATE
+			}
+		},
+		{ paranoid: true, tableName: 'user' }
+	);
+
+	return User;
+};
